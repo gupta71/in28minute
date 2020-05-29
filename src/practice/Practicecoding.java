f92f55ca3eb32905ddc956aebb48cce1868f88bc
cute puja <kpuja415@gmail.com>
2020-05-29 14:43:09 +0530

    code has been updated

---
+++ src/practice/Practicecoding.java
@@ -0,0 +1,14 @@
+package practice;
+
+public class Practicecoding {
+
+	public static void main(String[] args) {
+
+		int a = 10;
+		int b = 20;
+		int c = a + b;
+		System.out.println(c);
+
+	}
+
+}
